95f14a56a4e2ad5883daac6a6b0cf3c7f414bf86
Alex Liu <alex.liu@airbnb.com>
2016-10-17 13:42:13 -0700

    Initial Commit

---
+++ flicks/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  flicks
+//
+//  Created by alex_liu on 10/17/16.
+//  Copyright © 2016 alex_liu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
